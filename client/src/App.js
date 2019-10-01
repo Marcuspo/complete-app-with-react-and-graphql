74079056c2c43833ef924ede4aa0a3369a8660c8
Marcus <marcusporama@gmail.com>
2019-10-01 05:01:09 -0300

    iniciado o front-end com create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>Arruma essa coluna e vai beber água</h1>
+    </div>
+  );
+}
+
+export default App;
