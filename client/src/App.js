b4624cdb9c36df02becb275c07f2267bc984055d
Marcus <marcusporama@gmail.com>
2019-10-03 20:27:47 -0300

    Criação do booklist para listagem dos livros

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+//commponents
+import BookList from "./components/BookList";
+
 function App() {
   return (
     <div className="main">
       <h1>Arruma essa coluna e vai beber água</h1>
+      <BookList />
     </div>
   );
 }
