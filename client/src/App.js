b710f526a87cef8f476b9d8a196550df7de25bb7
Marcus <marcusporama@gmail.com>
2019-10-03 21:02:03 -0300

    Instalação do provider e realizando a consulta no banco setado em localhost400

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 //commponents
 import BookList from "./components/BookList";
 
+//ApolloClient setup
+const client = new ApolloClient({
+  uri: "http://localhost:400/graphql"
+});
+
 function App() {
   return (
-    <div className="main">
-      <h1>Arruma essa coluna e vai beber água</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="main">
+        <h1>Arruma essa coluna e vai beber água</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
